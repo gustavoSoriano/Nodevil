342402121df49be9f9b6472e5f2c2bb6c41b0b2b
Gustavo Soriano <sorianox2010@hotmail.com>
2019-01-09 22:40:19 -0200

    Update auth.js

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -2,26 +2,25 @@ const jwt      = require('jsonwebtoken')
 const config   = require('../../config/config.json')
 
 module.exports = () => {
-    let middlewares = {}
+   let middlewares = {}
 
-    middlewares.auth = (req, res, next) => {
-        if(req.path == '/login' || req.method == "OPTIONS" )return next()
-        const bearerHeader = req.headers['authorization']
-    
-        if(typeof bearerHeader !== 'undefined') 
-        {
-            const bearer      = bearerHeader.split(' ')
-            const bearerToken = bearer[1]
-            req.token         = bearerToken
-    
-            jwt.verify(bearerToken, config.secret, (err, authData) => {
-                return (err) ? res.status(401).json({status:false, message:'token inválido'}) : next()      
-            })
-        } 
-        else 
-            res.status(403).json({status:false, message:'Token é obrigatório'})
-    }
+   middlewares.auth = (req, res, next) => {
+      if (req.path == '/login' || req.method == "OPTIONS") return next()
+      const bearerHeader = req.headers['authorization']
 
-    return middlewares
+      if (typeof bearerHeader !== 'undefined') {
+         const [type, bearerToken] = bearerHeader.split(' ')
+
+         jwt.verify(bearerToken, config.secret, (err, authData) => {
+            req.token   = bearerToken
+            req.decoded = authData
+            return err ? res.status(401).json({ status: false, message: 'token inválido' }) : next()
+         })
+      }
+      else
+         res.status(403).json({ status: false, message: 'Token é obrigatório' })
+   }
+
+   return middlewares
 }
 
